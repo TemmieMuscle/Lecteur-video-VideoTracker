1f9a814b47280d8f4f8fcc83002ffa2bfbf9875f
Nathan DOÏMO <nathan.doimo@etu.u-bordeaux.fr>
2023-03-09 16:54:46 +0100

    video.py

--- src/models/Video.py
+++ src/models/Video.py
@@ -1,6 +1,58 @@
+import cv2
+import PIL.Image, PIL.ImageTk
+from tkinter import *
+
 class Video:
 
-    def __init__(self):
-        pass
+    def __init__(self, window, window_title):
+
+        self.window = window
+        self.window.title(window_title)
+        self.canvas = Canvas(window)
+        self.canvas.pack()
+        self.delay = 15   # ms
+        self.open_file()
+        self.play_video()
+        self.window.mainloop()
+
+    # Open video file
+    def open_file(self): 
+        self.pause = False
+        self.filename = '/media/nathan/MEGA/Cours/S2/Développement logiciel/GIT/videotracker_b1/src/models/compteur.mp4'
+        print(self.filename)
+        self.cap = cv2.VideoCapture(self.filename)
+        self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.canvas.config(width = self.width, height = self.height)
+        print(f"{self.width} : {self.height}")
 
 
+   # get only one frame    
+    def get_frame(self):   
+        try:
+            if self.cap.isOpened():
+                ret, frame = self.cap.read()
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        except:
+            print("end of the video !") # eventually make an error pop up ?
+
+    def play_video(self):
+        # Get a frame from the video source, and go to the next frame automatically
+        try :
+            ret, frame = self.get_frame()
+            if ret:
+                self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+                self.canvas.create_image(0, 0, image = self.photo, anchor = NW)
+            if not self.pause:
+                self.window.after(self.delay, self.play_video)   
+        except :
+            print(" End of the video")
+        
+
+        # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.cap.isOpened():
+            self.cap.release()
+
+
+Video(Tk(), "Video Tracker")
