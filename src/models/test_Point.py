83bceac2008166274fe6b8a0827fbf2d2a1db329
Nathan DOÏMO <nathan.doimo@etu.u-bordeaux.fr>
2023-04-13 14:39:01 +0200

    Created yml file for IC

---
+++ src/models/test_Point.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+sys.path.append("../")
+
+from Point import Point
+
+import random
+
+class Test_FileRepo(unittest.TestCase):
+
+    def setUp(self) -> None:
+        # creer instance de Point
+        pass
+
+    def tearDown(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+    
+    
