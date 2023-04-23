aa55f2d77301b64f32d163f73e36eafd6d2f9212
Léonard Bettcher <leonard.bettcher@etu.u-bordeaux.fr>
2023-04-24 00:00:58 +0200

    version finale

--- src/controllers/Controller.py
+++ src/controllers/Controller.py
@@ -11,12 +11,7 @@ class Controller:
         # Menu configuration
         self.view.fileMenu.entryconfig(0, command=self.loadVideo) 
         self.view.fileMenu.entryconfig(1, command=self.playAndLoadVideo) 
-<<<<<<< HEAD
-        self.view.fileMenu.entryconfig(3, command=self.FileRepo.save) ######## TO BE DISCONTINUED mais on verra quand on fera les points
-=======
-        self.view.fileMenu.entryconfig(3, command=self.FileRepo.save_data)
-        
->>>>>>> 86dc013c2fd1c03ce760923fa9f6fc9cc2c023db
+        self.view.fileMenu.entryconfig(3, command=self.FileRepo.save) ######## TO BE DISCONTINUED mais on verra quand on fera les points        
         # Video action buttons
         self.view.play_btn.config(command=self.video.pause_video)
         self.view.skipBackward_btn.config(command=self.video.skip_to_first_frame)
