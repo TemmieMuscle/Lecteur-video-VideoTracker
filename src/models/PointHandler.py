5d32e249400a1601ca3978dd33e6975b1262d8d5
Léonard Bettcher <leonard.bettcher@etu.u-bordeaux.fr>
2023-05-04 15:05:21 +0200

    class PointHandler crée

---
+++ src/models/PointHandler.py
@@ -0,0 +1,18 @@
+import math
+import Point
+
+class PointHandler():
+    def __init__(self):
+        self.tabPoint=[]
+        pass
+
+    def addPoint(self):
+        pass
+
+    # créer le graphe et l'affiche
+    def printGraphe(self):
+        pass
+
+    def getTab(self):
+        return self.tabPoint
+    
